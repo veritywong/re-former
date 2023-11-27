e5c5fed215dafcd4a41438fa56eb55e1c9ef921d
Verity <veritywong90@gmail.com>
2023-11-27 09:30:09 +0000

    rails form with form_tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,5 +15,6 @@ class UsersController < ApplicationController
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
+        #params.permit(:username, :email, :password)
     end
 end
