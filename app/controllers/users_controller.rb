ade95e7f373718487d7268330eb70b3730c2b0cf
Verity <veritywong90@gmail.com>
2023-12-08 13:30:26 +0000

    form update functionality wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,26 @@ class UsersController < ApplicationController
       end
     end
 
+    def edit
+      @user = User.find(params[:id])
+      if @user
+        render :edit
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        # handle successful update, maybe redirect to show page or another page
+        redirect_to user_path(@user), notice: 'User was successfully updated.'
+      else
+        # handle validation errors, re-render the edit form
+        render :edit
+      end
+    end
+
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
